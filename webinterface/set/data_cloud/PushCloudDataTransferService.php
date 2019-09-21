2a498acfd89a02c18bd8eaa39eaeaee8ea6c7d3d
Johannes Strasser <johannes.strasser@wistcon.at>
2019-09-21 21:22:53 +0200

    add I2C-error comp

--- webinterface/set/data_cloud/PushCloudDataTransferService.php
+++ webinterface/set/data_cloud/PushCloudDataTransferService.php
@@ -34,6 +34,8 @@ while ($loopstatus){
 	set_time_limit(10);
 
 	$t = 0;
+	$I2C_error_humidity = -1;
+	$I2C_error_temp = -1;
 	$transfer = array();
 
 	for($i=0;$i<$NumberofDatasets;$i++){
@@ -50,17 +52,31 @@ while ($loopstatus){
 
 		if($SendData != NULL){
 			$transfer[$t] = $SendData;
-			//set timestamp to calculate next push ivent
-			$FetchXMLData[$i]["timestamp"] = $SendData[3];
+			switch ($t) {
+				case 0:
+					if ($transfer[0][2] > 1){
+						$I2C_error_humidity = 1;
+					}
+					break;
+				case 1:
+					if ($transfer[1][2] > -40){
+						$I2C_error_temp = 1;
+					}
+			}
 			$t+=1;
-			
 		}
 	}
+
 		if ($transfer != NULL){
-		$return = $DataCloudClass->SendData($transfer);
-		$return_arr = $DataCloudClass->DatabaseSendData_return($return);
-		$DataCloudClass->XMLDataWriteSendStatus($return_arr['database_write']);
-	}
+			if ($I2C_error_humidity == 1 && $I2C_error_temp == 1){
+				//set timestamp to calculate next push ivent
+				$FetchXMLData[0]["timestamp"] = time();
+				$FetchXMLData[1]["timestamp"] = time();
+				$return = $DataCloudClass->SendData($transfer);
+				$return_arr = $DataCloudClass->DatabaseSendData_return($return);
+				$DataCloudClass->XMLDataWriteSendStatus($return_arr['database_write']);
+			}
+		}
 	
 /*	print_r($transfer);
 	ob_flush();
@@ -76,3 +92,4 @@ while ($loopstatus){
         flush();
  */
 ?>
+
